3621e76642f77addb7eb3d9f8bf18a8d75b92d2e
Jeena01 <jeenajavahar@gmail.com>
2024-11-13 15:59:02 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import TabView from './components/TabView';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        
         <TabView/>
-      </header>
+      
     </div>
   );
 }
