291d0da1fa5a467395c03b00d4acb6d75a84b76c
Jeena01 <jeenajavahar@gmail.com>
2024-11-13 15:54:34 -0800

    Update TabView.js

--- src/components/TabView.js
+++ src/components/TabView.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Tabs, Tab, Box } from '@mui/material';
-import './TabView.css'; // Optional for additional custom styling
 
 function TabView() {
   // State to track the active tab
